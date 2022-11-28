8efaaf6ee1b7c86a2de53cc58dc20890e750698c
analogica <superanalogica@gmail.com>
2022-11-28 22:35:37 +0100

    agrego línea 10

---
+++ ProjectGitAna/src/GitClass.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// Proyecto Git
+		
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
+		
+		scanner.close();
+		
+
+	}
+
+}
